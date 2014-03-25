34e16db2cc8552e86007a5a3630c222d26b4327f
sholto maud <sholto.maud@gmail.com>
2014-03-25 13:56:45 +1000

    Initial commit

---
+++ lib/Existence.pm
@@ -0,0 +1,236 @@
+{
+package Existence;
+  #use lib "$ENV{INIPATH}chr\\lib\\Hydstra";
+  use Moose;
+  use Moose::Util::TypeConstraints;
+
+  #Chromicon modules
+  use Logger;
+
+  #Hydstra modules
+  use HydDllp;
+
+  #Global Variable
+  my $DEFBUFFER        = 1950;
+
+=head1 Existence
+
+Custom module to check existence of records
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Use this module to check existence of records
+     
+=cut 
+  
+has 'table_name'  => ( is => 'rw', isa => 'Str'); 
+has 'sitelist_filter'  => ( is => 'rw', isa => 'Str'); 
+has 'company_name'  => ( is => 'rw', isa => 'Str'); 
+
+  sub baseline_exists{
+    my $c = shift;
+    my $site = shift;
+    my %params;
+    $params{table_name} = 'areasmt';
+    $params{return_type} = 'array';
+    $params{sitelist_filter} = $site;
+    my $dllp = HydDllp->New();
+    my @d = $dllp->get_db_info(
+      { 'function' => 'get_db_info',
+        'version'  => 3,
+        'params'   => \%params
+      },
+      $DEFBUFFER
+    );
+    $dllp->Close();
+    #Prt( '-P', NowStr() . "   - return\n".HashDump(\%hydtables)."]\n" );
+    #print Dumper(\%d);
+    if ( defined $d[0] ){
+      return 1; 
+    }
+    else {
+      return 0;
+    }
+  }
+  
+  sub table_keys{
+    my $c = shift;
+    my $table = shift;
+    my %params;
+    my @complex_filter;
+    my $key_no = 0;
+    
+    $params{complex_filter} = [
+      { 
+          'fieldname'=>'dbname',
+          'operator'=>'EQ',
+          'value'=>$table
+      },
+      {
+          'combine'=>'AND',
+          'fieldname'=>'keyfld',
+          'operator'=>'EQ',
+          'value'=>'true'
+      }
+    ];
+    
+    my @field_list = ('fldname');
+    $params{table_name} = 'mastdict';
+    $params{return_type} = 'hash';
+    $params{field_list} = \@field_list;
+    
+    my $dllp = HydDllp->New();
+    my %d = $dllp->get_db_info(
+      { 'function' => 'get_db_info',
+        'version'  => 3,
+        'params'   => \%params
+      },
+      $DEFBUFFER
+    );
+    $dllp->Close();
+    my %return_data;
+    
+    foreach my $table ( keys %d ){
+      foreach my $field_key_no ( keys %{$d{$table}} ){
+        foreach my $field ( keys %{$d{$table}{$field_key_no}} ){ 
+          my $field_name = lc($field);
+          $return_data{$field_name}++;
+        }
+      }
+    }    
+    return %return_data;
+    #return %d;
+  }
+  
+ 
+=cut  
+  sub key_exists{
+    my $c = shift;
+    my $table = shift;
+    #my %ky = shift;
+    my $kesy = shift;
+    my %ky = %{$kesy};
+    my %params;
+    my @complex_filter;
+    my $key_no = 0;
+    
+    foreach my $key ( keys %ky){
+      $key_no++;
+      my %complex;
+      #print "key no [$key_no] fieldname [$key] value [$ky{$key}]\n";
+      $complex{fieldname} = $key;
+      $complex{operator} = 'EQ';
+      $complex{value} = $ky{$key};
+      
+      if ($key_no > 1){
+        $complex{combine} = 'AND';
+      }
+      
+      push (@complex_filter, \%complex );
+    }
+    #print "Done. Getting complext filter for table [$table]. It looks like:\n";
+    #foreach my $complex_no ( 0..$#complex_filter){
+    #  print "  . complex #[$complex_no] = [$complex_filter[$complex_no]]\n";
+    #}
+    
+    $params{table_name} = $table;
+    $params{return_type} = 'array';
+    $params{complex_filter} = \@complex_filter;
+    
+    my $dllp = HydDllp->New();
+    my @d = $dllp->get_db_info(
+      { 'function' => 'get_db_info',
+        'version'  => 3,
+        'params'   => \%params
+      },
+      $DEFBUFFER
+    );
+    $dllp->Close();
+    #Prt( '-P', NowStr() . "   - return\n".HashDump(\%hydtables)."]\n" );
+    #print Dumper(\%d);
+    if ( defined $d[0] ){
+      return 1; 
+    }
+    else {
+      return 0;
+    }
+    
+  }
+
+
+
+=head1 AUTHOR
+
+Sholto Maud, C<< <sholto.maud at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs in the issues wiki.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Existence
+
+=over 4
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Sholto Maud.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+1; # End of Existence
